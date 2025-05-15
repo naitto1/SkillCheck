1d01e66ebe61ec27e4b3d3d889f541855ada2404
naitto1 <yzahrani28@gmail.com>
2025-05-15 15:36:01 -0400

    added init

---
+++ app/scraper.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://www.linkedin.com/jobs/search/?currentJobId=4225916174&distance=25&geoId=103644278&keywords=Data%20Analyst&origin=JOBS_HOME_KEYWORD_HISTORY&refresh=true"
+def extract():
+
