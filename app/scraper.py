271abc742a8fe4153ab3c02b9ed96d6df9a4d7b3
naitto1_w <yzahrani28@gmail.com>
2025-05-15 16:52:31 -0400

    added selenium and updated req

--- app/scraper.py
+++ app/scraper.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from selenium import webdriver
+driver = webdriver.Chrome()
+
 
 url = "https://www.linkedin.com/jobs/search/?currentJobId=4225916174&distance=25&geoId=103644278&keywords=Data%20Analyst&origin=JOBS_HOME_KEYWORD_HISTORY&refresh=true"
 def extract():
