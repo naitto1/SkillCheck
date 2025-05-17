51a5e02c047c6904ec30f956290778b048fbee4a
naitto1_w <yzahrani28@gmail.com>
2025-05-17 17:01:18 -0400

    aa

--- app/scraper.py
+++ app/scraper.py
@@ -1,10 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from selenium import webdriver
-driver = webdriver.Chrome()
+import csv
+import time
 
+# job_data = []
+# url = "https://www.linkedin.com/jobs/search/?currentJobId=4225916174&distance=25&geoId=103644278&keywords=Data%20Analyst&origin=JOBS_HOME_KEYWORD_HISTORY&refresh=true"
 
-url = "https://www.linkedin.com/jobs/search/?currentJobId=4225916174&distance=25&geoId=103644278&keywords=Data%20Analyst&origin=JOBS_HOME_KEYWORD_HISTORY&refresh=true"
-def extract():
+# try:
+#     response = requests.get(url)
+#     response.raise_for_status()
 
+#     soup = BeautifulSoup(response.content, 'html-parser')
+
+#     job_listings = soup.find_all('h1', class='t-24 t-bold inline')
+    
+#     for job in job_listings:
+#         try:
+#             title_element = job.find('h2')
+
+def extract(job_title):
+    
